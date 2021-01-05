bfad1d1bc34e4c3feeb804bdfa8f8d40fb9f0efe
BaeSukkyoung <61176230+BaeSukkyoung@users.noreply.github.com>
2021-01-05 18:05:03 +0900

    Delete request.py

--- request.py
+++
@@ -1,7 +0,0 @@
-import os
-import requests
-
-image_path = os.getcwd() + '/sample_image.jpeg'
-
-resp = requests.post("http://localhost:5000/predict",
-                     files={"file": open('sample_image.jpeg','rb')})
